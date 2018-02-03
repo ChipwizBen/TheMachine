424c7bb1d83462eeafd7372b975f59a6bd9f2d4d
Ben Schofield <ben@nwk1.com>
2018-02-03 16:13:34 +1300

    Check update process.

---
+++ HTTP/check-update.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl -T
+
+use strict;
+use lib qw(/opt/TheMachine/Modules/);
+
+use DBI;
+
+require './common.pl';
+
+my $DB_Connection = DB_Connection();
+my ($Version, $Latest_Version, $URL, $Notification) = Version('Version_Check');
+
+if (!$URL) {print "No URL to check! You should manually add a URL to the 'version' table in the database.\n"; exit(1);}
+
+print "Checking version and notifications from $URL...\n";
+
+my $Returned = `curl -sS $URL 2>&1`;
+
+if ($Returned =~ /URL/) {
+	$Latest_Version = $Returned;
+		$Latest_Version =~ s/.*Version='(.*?)'.*/$1/gs;
+	$Notification = $Returned;
+		$Notification =~ s/.*Notification='(.*?)'.*/$1/gs;
+	$URL = $Returned;
+		$URL =~ s/.*URL='(.*?)'.*/$1/gs;
+
+	if ($Version eq $Latest_Version) {
+		print "Installed Version:\t$Version\n";
+		print "Latest Version:\t\t$Latest_Version\n";
+		print "You are using the latest version\n";
+	}
+	else {
+		print "Installed Version:\t$Version\n";
+		print "Latest Version:\t\t$Latest_Version\n";
+		print "An update is available!\n";
+
+		my $Version_Check = $DB_Connection->prepare("UPDATE `version` SET
+		`Latest_Version` = ?,
+		`URL` = ?
+		WHERE 1=1");
+		$Version_Check->execute($Latest_Version, $URL);
+
+	}
+	if ($Notification) {
+		print "Notification: $Notification\n";
+		my $Notification_Update = $DB_Connection->prepare("UPDATE `version` SET
+		`Notification` = ?
+		WHERE 1=1");
+		
+		$Notification_Update->execute($Notification);
+	}
+	exit(0);
+}
+else {
+	print "Something went wrong. This is what we got:\n$Returned\n";
+	exit(1);
+}
+
+
+
+1;
